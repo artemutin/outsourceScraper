a17cd2f132e5e76eaa762ab464a9048cf2558ff5
Artem Utin <artemutin@yandex.ru>
2016-06-16 00:51:50 +1000

    date parse refactor and proved

--- tests/unit/farpost_catalog.py
+++ tests/unit/farpost_catalog.py
@@ -32,4 +32,13 @@ class FarpostCatalog(unittest.TestCase):
         self.assertEqual(ad['labeledCategory'], "Бухгалтерско-юридическая компания")
         self.assertEqual(ad['address'], {'region': 25, 'city': "Владивосток", 'rest': " пр-кт Красного Знамени, 59"})
 
+        # checked all dates, just in case
+        dates = [date(2016, 5, 30), date(2016, 4, 17), date(2016, 4, 24), date(2016, 6, 1), date(2016, 3, 23), date(2016, 4, 22),
+                 date(2016, 5, 25), date(2016,4,13), date(2016,6,7), date(2016,5,11),date(2016,3,28), date(2016,3,23),
+                 date(2016,3,23),date(2016,3,2),date(2016,6,10),date(2016,5,26),date(2016,5,23),date(2016,5,20),date(2016,5,16),
+                 date(2016,5,11),date(2016,5,6),date(2016,5,6),date(2016,4,28),date(2016,4,13),date(2016,3,23),date(2016,3,9),
+                 date(2016,6,7),date(2016,6,3),date(2016,5,30),date(2016,5,27)]
+        for i in range(0, 30):
+            self.assertEqual(ad_list[i]['renewDate'], dates[i])
+
 
